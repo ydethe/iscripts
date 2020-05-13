fceba164d23390b339d62c75657bfafce515cf39
Yann de Thé <ydethe@gmail.com>
2020-05-13 21:30:00 +0200

    sync vers johncloud

--- synchro_music.sh
+++ synchro_music.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
-rsync -mvP --exclude '*.DS_Store*' --exclude '@ea*' --chmod=Du=rwx,Dg=rwx,Do=rwx,Fu=rw,Fg=rw,Fo=rw $HOME/Music/iTunes/iTunes\ Media/Music/ eru:/volume1/music/ 
+# rsync -mvP --exclude '*.DS_Store*' --exclude '@ea*' --chmod=Du=rwx,Dg=rwx,Do=rwx,Fu=rw,Fg=rw,Fo=rw $HOME/Music/iTunes/iTunes\ Media/Music/ eru:/volume1/music/ 
+rsync -mvP --exclude '*.DS_Store*' --exclude '@ea*' --chmod=Du=rwx,Dg=rwx,Do=rwx,Fu=rw,Fg=rw,Fo=rw $HOME/Music/iTunes/iTunes\ Media/Music/ ks:/home/user-data/owncloud/yann\@johncloud.fr/files/Musique/
 
 
