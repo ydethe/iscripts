2fb45210280ffdc3520a6f882aebb626adecdb74
Yann de Thé <ydethe@gmail.com>
2021-01-31 17:53:35 +0100

    Synchro musique OK

--- synchro_music.sh
+++ synchro_music.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-rsync -rmvP --exclude '.DS_Store' --exclude '@ea*' --chmod=Du=rwx,Dg=rwx,Do=rwx,Fu=rw,Fg=rw,Fo=rw $HOME/Music/iTunes/iTunes\ Media/Music/ eru:/volume1/music/ 
+rsync -rmvP --exclude '.DS_Store' --exclude '@ea*' --chmod=Du=rwx,Dg=rwx,Do=rwx,Fu=rw,Fg=rw,Fo=rw $HOME/Music/iTunes/iTunes\ Media/Music/. eru:/volume1/music
 # rsync -mvP --exclude '*.DS_Store*' --exclude '@ea*' --chmod=Du=rwx,Dg=rwx,Do=rwx,Fu=rw,Fg=rw,Fo=rw $HOME/Music/iTunes/iTunes\ Media/Music/ ks:/home/user-data/owncloud/yann\@johncloud.fr/files/Musique/
