022745a90d6b771d2f5a3ac6600510b6db4b81b8
Yann de The <ydethe@gmail.com>
2023-12-19 22:15:32 +0100

    Updated sync music

--- synchro_music.sh
+++ synchro_music.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
-rsync -rmvP --exclude '.DS_Store' --exclude '@ea*' --chmod=Du=rwx,Dg=rwx,Do=rwx,Fu=rw,Fg=rw,Fo=rw $HOME/Music/iTunes/iTunes\ Media/Music/. eru:/volume1/music
+# rsync -rmvP --exclude '.DS_Store' --exclude '@ea*' --chmod=Du=rwx,Dg=rwx,Do=rwx,Fu=rw,Fg=rw,Fo=rw $HOME/Music/iTunes/iTunes\ Media/Music/. eru:/volume1/music
 # rsync -mvP --exclude '*.DS_Store*' --exclude '@ea*' --chmod=Du=rwx,Dg=rwx,Do=rwx,Fu=rw,Fg=rw,Fo=rw $HOME/Music/iTunes/iTunes\ Media/Music/ ks:/home/user-data/owncloud/yann\@johncloud.fr/files/Musique/
+rsync -avz --exclude '.DS_Store' --exclude '@ea*' --no-perms --no-owner --no-group --ignore-existing --omit-dir-times /Users/ydethe/Music/Music/Media.localized/Music/ johncloud:johncloud_data/nextcloud/data/data/ydethe/files/Musique
